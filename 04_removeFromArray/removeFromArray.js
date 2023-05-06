10087b84f4b35fe7d8195c3bea485abb8487f379
Mauricio López <maurilop92@gmail.com>
2023-05-06 14:30:57 -0300

    Solve excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let newArray = [];
 
+  for (let i = 0; i < arguments[0].length; i++) {
+    let argsToCheck = 0;
+    for (let j = 1; j < arguments.length; j++) {
+      if (arguments[0][i] === arguments[j]) {
+        continue;
+      } else {
+        argsToCheck++;
+      }
+    }
+    if (argsToCheck === arguments.length - 1) {
+      newArray.push(arguments[0][i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
