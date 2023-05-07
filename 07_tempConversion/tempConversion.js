e61a082c78e078b6ee26aebf411ea21b675df35a
Mauricio López <maurilop92@gmail.com>
2023-05-07 17:34:37 -0300

    Solve excercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // Formula: x °F ≘ (x − 32) × 5/9 °C
+  const tempInCelcius = (temp - 32) * (5 / 9);
+  return Number(tempInCelcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // Formula: 	x °C ≘ (x × 9/5 + 32) °F
+  const tempInFahrenheit = temp * (9 / 5) + 32;
+  return Number(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
