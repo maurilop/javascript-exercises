3fd371374d392d86c4a65f7c72966f09d814d69c
Mauricio López <maurilop92@gmail.com>
2023-09-16 20:53:23 -0300

    Solve excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  let result = 0;
+  for (let i = 0; i < arguments[0].length; i++) {
+    result += arguments[0][i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function () {
+  let result = arguments[0][0];
+  for (let i = 1; i < arguments[0].length; i++) {
+    result *= arguments[0][i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  // 4 * 4 * 4
+  let result = base;
+  for (let i = 1; i < power; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+  let result = 1;
+  for (let i = 1; i <= arguments[0]; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
