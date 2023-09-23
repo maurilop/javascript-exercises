1bbb3f224e8680925a5a8f51c99506cb89217e92
Mauricio López <maurilop92@gmail.com>
2023-09-23 20:45:03 -0300

    Solve excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  let fs = s.split(" ").join("").toLowerCase().replace(/\W/g, "");
+  let sl = fs.length - 1;
+  console.log(fs);
+  for (i = 0; i <= sl; i++) {
+    if (fs[i] == fs[sl - i]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
