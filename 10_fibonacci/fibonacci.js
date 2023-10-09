801487399b01cd9692617d00381c3e8b457774fc
Mauricio López <maurilop92@gmail.com>
2023-10-09 20:32:53 -0300

    Solve excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const a = [1, 1];
+  if (num < 1) {
+    return "OOPS";
+  } else if (num <= 2) {
+    return 1;
+  } else {
+    for (let i = 2; i <= num; i++) {
+      a[i] = a[i - 1] + a[i - 2];
+    }
+    return a[num - 1];
+  }
 };
+
+/*
+F(n) = F(n-1) + F(n-2)
+
+
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
