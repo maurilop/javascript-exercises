1bc7ea0f622b4f01b444dae7aa4985b82d64f79a
Mauricio López <maurilop92@gmail.com>
2023-10-17 19:56:02 -0300

    Solve excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let d = new Date();
+  let currentYear = d.getFullYear();
+  let arrSorted = arr.sort(function (a, b) {
+    if (a.yearOfDeath == undefined) {
+      a.yearOfDeath = currentYear;
+    }
+    if (b.yearOfDeath == undefined) {
+      b.yearOfDeath = currentYear;
+    }
+    if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return arrSorted[0];
 
+  /* const arrReduced = arr.reduce((total, people) => {
+    return total + (people.yearOfDeath - people.yearOfBirth);
+  }, 0); */
 };
 
 // Do not edit below this line
