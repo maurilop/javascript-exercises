befc442fab3a51659c562b05bc82504e9ffc6418
Mauricio López <maurilop92@gmail.com>
2023-10-17 18:58:41 -0300

    Solve excercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (a) {
+  let newArr = [];
+  for (let i = 0; i < a.length; i++) {
+    newArr[i] = a[i].title;
+  }
+  return newArr;
 };
 
 // Do not edit below this line
